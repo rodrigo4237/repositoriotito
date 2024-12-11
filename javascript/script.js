bf9db8c97c75a7070c3e60f4006a06f28d7e0e5c
rodrigo4237 <rodrigaldamez8@gmail.com>
2024-12-11 13:42:13 -0300

    cambios

--- javascript/script.js
+++ javascript/script.js
@@ -14,34 +14,10 @@ if (close) {
     })
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+const user = document.getElementsById('userBtn');
 
-    document.querySelector(".form-box").addEventListener("submit", function(event) {
-        event.preventDefault(); 
-        
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
-
-        const validUsername = "usuario@gmail.com";
-        const validPassword = "123456";
-
-        if (username === validUsername && password === validPassword) {
-            alert("Inicio de sesión exitoso.");
-            
-            window.location.href = "/administrador.html";
-        } else {
-            alert("Email o contraseña incorrectos.");
-        }
-    });
-});
-
-const showPassword = document.getElementById("showPassword");
-const passwordField = document.getElementById("password");
-
-showPassword.addEventListener("change", function() {
-    if (showPassword.checked) {
-        passwordField.type = "text";
-    } else {
-        passwordField.type = "password";
-    }
-});
+if (user) {
+    user.addEventListener('click', () => {
+        loadPage('user');
+        })
+}
